779e62b9373e019e202280322ff1af4a38a29ee1
Umar Sajjad <umar.sajjad8@gmail.com>
2024-05-22 12:37:40 +0500

    done

--- hello_new.py
+++ hello_new.py
@@ -1,9 +1,14 @@
 print("Hello World")
 
 
-def add(a: float, b: float) -> float:
+def divide(a: float, b: float) -> float:
     return a/b
 
 
+def add(a: float, b: float) -> float:
+    return a + b
+
+
 if __name__ == "__main__":
+    print(divide(5, 10))
     print(add(5, 10))
