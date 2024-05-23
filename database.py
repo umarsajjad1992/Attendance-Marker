01f6bd9d12f03be012545f2dbda7dba017e8877b
Umar Sajjad <umar.sajjad8@gmail.com>
2024-05-23 13:28:41 +0500

    Added role in database table

--- database.py
+++ database.py
@@ -8,7 +8,8 @@ def initialize_db():
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY,
             username TEXT UNIQUE NOT NULL,
-            password TEXT NOT NULL
+            password TEXT NOT NULL,
+            role TEXT NOT NULL DEFAULT "user"
         )
     ''')
     c.execute('''
